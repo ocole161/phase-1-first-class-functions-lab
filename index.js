e586ea5d551418ddb4cbabef2697b46c5403dfaf
Owen Cole <ocole161@gmail.com>
2022-12-15 15:00:18 -0700

    commit tree e6db9bde638498a5de54699397e9e2ac6fcde47c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = [`a`,`b`,`c`,`d`,`e`,`f`]
+
+const returnFirstTwoDrivers = (drivers) => {
+    const firstTwo = drivers.slice(0,2) 
+    
+    console.log(firstTwo)
+
+    return firstTwo
+  
+}
+returnFirstTwoDrivers(drivers)
+
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastTwo = drivers.slice(-2)
+    return lastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return (fare) => multiplier * fare
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(xyz, asdf) { 
+    return asdf(xyz)
+ }
